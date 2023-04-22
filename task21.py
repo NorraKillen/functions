f143b1b63f9a242fb5396d6f6b3b9cf31afde81c
Evgenii Maznyak <NorraKillen@yandex.ru>
2023-04-22 15:14:07 +0200

    list of primary cities with a population of more than 10000000 people

---
+++ task21.py
@@ -0,0 +1,26 @@
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+map_result = reduce(lambda x, y: x +  y + ', ' , sorted(list(map(lambda num: num[0], list(filter(lambda num: num[1] > 10000000 and num[2] == 'primary', data))))), 'Cities: ')
+
+print(map_result[:-2])
