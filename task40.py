0b14e788d1bb17e8646ecb56fb32873b6706ee5b
Evgenii Maznyak <NorraKillen@yandex.ru>
2023-05-07 20:37:16 +0200

    Honors students

--- task40.py
+++ task40.py
@@ -1,6 +1,6 @@
 n = int(input())
 l = all([any(map(lambda x: x == '5', [input()[-1] for j in range(int(input()))])) for i in range(n)])
 if l == True:
-    print('YES')
+    print( 'YES')
 else:
     print('NO')
