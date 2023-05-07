cfde9fd6857fe5544eca3f3297264624f5425948
Evgenii Maznyak <NorraKillen@yandex.ru>
2023-05-07 20:33:36 +0200

    ignore_command

---
+++ task34.py
@@ -0,0 +1,3 @@
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+    return any(map(lambda x: x in command, ignore))
